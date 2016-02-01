3d48a2e7a94c1b8b4ef7b904d31b95bb50e838c4
Arno Thys <arno.thys@hotmail.com>
2016-02-01 11:20:15 +0100

    Threads first half done

---
+++ Threads/Threads/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    static class Controller
+    {
+        internal static void BigMethodThatDoesAlotOfThings()
+        {
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
